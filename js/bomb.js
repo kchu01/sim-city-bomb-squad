6dcb9f3d251dbfb883a486c96308fc92011c8994
h64 <hhongnr@gmail.com>
2021-01-28 15:34:57 -0800

    Add app state, DOM references, and function stubs

--- js/bomb.js
+++ js/bomb.js
@@ -1,11 +1,56 @@
 /*----- constants -----*/
+const INITIAL_TIME = 30;
 
 /*----- app's state (variables) -----*/
+let timeRemaining = 0;
+let gameOver = false;
+let wiresToCut = [];
+let wireState = {
+    blue: false,
+    green: false,
+    red: false,
+    white: false,
+    yellow: false
+}
 
-/*----- cached element references -----*/
+/*----- DOM references -----*/
+let backgroundEl = null;
+let wires = [];
+let resetBtnEl = null;
+let timerEl = null;
 
-/*----- event listeners -----*/
+/*----- Function and Game Logic -----*/
+function updateClock() {
+
+}
+
+function resetGame() {
+
+}
+
+function initializeGame() {
+
+}
+
+function cutWire() {
+
+}
+
+function endGame() {
+
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('loaded!');
+    // Hook up DOM references to the variables
+    backgroundEl = document.querySelector('main');
+    let wireBoxEl = document.querySelector('#wirebox');
+    wires = wireBoxEl.children;
+    resetBtnEl = document.querySelector('button');
+    timerEl = document.querySelector('p');
+
+
+    // Register Event Listeners
+    resetBtnEl.addEventListener('click', resetGame);
+    wireBoxEl.addEventListener('click', cutWire);
 })
